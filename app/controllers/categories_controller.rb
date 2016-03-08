2ad896e8f591612515b8ad7fb366acb05e81b74e
Izad Che Muda <izad@selklik.com>
2016-03-08 10:12:54 +0800

    timestamp

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,33 +3,40 @@ require 'open-uri'
 class CategoriesController < ApplicationController
 
   def index
-=begin
-    categories = []
-
-    doc = Nokogiri::HTML(open(base_url))
-
-    doc.css('.megamenu li').each do |node|
-      a = node.at_css('a')
-
-      next if %w(Berita Video).include? a.content
-      next unless node[:id].present? || a[:href].include?('berita/')
-      break if a.content == 'Lain-lain'
-
-      categories << {
-        name: a.content,
-        path: URI.parse(a[:href]).path
-      }
-    end
-
-    categories[0], categories[1] = categories[1], categories[0]
-
-    render json: categories
-=end    
   end
 
   def show
-    #render plain: params[:slug]
+    if params[:second].present?
+      path = "#{params[:first]}/#{params[:second]}"
+    else
+      path = params[:first]
+    end
 
+    doc = Nokogiri::HTML(open("#{base_mobile_url}/#{path}"))
+
+    headlines = []
+
+    doc.css('.linklist li').each do |node|
+      h2 = node.at_css('h2')
+      img = node.at_css('img')
+
+      headline = {
+        id: h2.at_css('a')[:href].split('/').last.split('-').last.gsub('.', '_'),
+        title: h2.content,
+        timestamp: node.at_css('span').content
+      }
+
+      if img.present?
+        headline[:photo] = "#{base_url}#{img[:src].gsub('box_80', 'landscape_650')}"
+      else
+        headline[:photo] = nil
+      end
+
+      headlines << headline
+    end
+
+    render json: headlines
+=begin
     if params[:second].present?
       path = "#{params[:first]}/#{params[:second]}"
     else
@@ -60,6 +67,8 @@ class CategoriesController < ApplicationController
     end
 
     render json: headlines
+=end
+
   end
 
   def video
@@ -70,6 +79,10 @@ class CategoriesController < ApplicationController
 
     def base_url
       "http://www.utusan.com.my"
+    end
+
+    def base_mobile_url
+      "http://m.utusan.com.my"
     end
 
 end
